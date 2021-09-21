f8545d1c5c86eacbcede747c9c962dc8f4426f62
Giles Cardenas <89228047+GilesC90@users.noreply.github.com>
2021-09-21 13:19:00 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("page loaded...");
+
+
+
+function startVideo(element) {
+    var x = document.querySelector('#mute')
+    x.play();
+}
+
+function pauseVideo(element) {
+    var x = document.querySelector('#mute')
+    x.pause();
+}
